554465c1d53696dfa9de9d06def311ba124308d4
Benjamin Pannell <benjamin@pannell.dev>
2023-01-02 20:55:59 +0000

    chore: Bump frontend dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require.config({
     paths: {
-        "vue-class-component": "https://cdn.jsdelivr.net/npm/vue-class-component@7.1.0/dist/vue-class-component.min",
-        "highlight-js": "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min",
+        "vue-class-component": "https://cdn.jsdelivr.net/npm/vue-class-component@7.2.6/dist/vue-class-component.min",
+        "highlight-js": "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min",
         "markdown-it": "https://cdnjs.cloudflare.com/ajax/libs/markdown-it/13.0.1/markdown-it.min",
         "text": "https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
     },
@@ -13,37 +13,37 @@ require.config({
     packages: [
         {
             name: "vue-module",
-            location: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10",
+            location: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.14",
             main: "vue"
         },
         {
             name: "vue-router-module",
-            location: "https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.1.3",
+            location: "https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.6.5",
             main: "vue-router"
         },
         {
             name: "vuex",
-            location: "https://cdnjs.cloudflare.com/ajax/libs/vuex/3.1.1",
+            location: "https://cdnjs.cloudflare.com/ajax/libs/vuex/3.6.2",
             main: "vuex"
         },
         {
             name: "dayjs",
-            location: "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.8.16",
+            location: "https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.7",
             main: "dayjs.min"
         },
         {
             name: "ELEMENT",
-            location: "https://cdnjs.cloudflare.com/ajax/libs/element-ui/2.12.0",
+            location: "https://cdnjs.cloudflare.com/ajax/libs/element-ui/2.13.2",
             main: "index"
         },
         {
             name: "Msal",
-            location: "https://alcdn.msauth.net/lib/1.1.3/js",
+            location: "https://alcdn.msauth.net/lib/1.4.17/js",
             main: "msal"
         },
         {
             name: "spark-md5",
-            location: "https://cdnjs.cloudflare.com/ajax/libs/spark-md5/3.0.0",
+            location: "https://cdnjs.cloudflare.com/ajax/libs/spark-md5/3.0.2",
             main: "spark-md5.min"
         }
     ],
